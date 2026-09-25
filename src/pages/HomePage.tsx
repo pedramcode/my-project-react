3bdac5cc304b2d6101557dd420e793641dd2cbad
Pedram Dehghanpour <dev.dehghanpour@gmail.com>
2026-09-25 10:58:39 +0330

    HomePage initialization

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+export function HomePage() {
+    return (
+        <div className="
+            w-dvw 
+            h-dvh 
+            flex
+            flex-col
+            bg-red-400
+        ">
+            <div></div>
+        </div>
+    )
+}
